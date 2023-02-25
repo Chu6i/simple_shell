9dee185c9e4b0829d4963d8d26eb3921274b5be2
Bright Yanchirah <nanabryte.nb@gmail.com>
2023-02-25 15:47:46 -0800

    .

--- builtin1.c
+++ builtin1.c
@@ -28,6 +28,8 @@ int unset_alias(info_t *info, char *str)
 		return (1);
 	y = *x;
 	*x = 0;
+	zen = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
 
 	*x = y;
 	return (zen);
@@ -109,4 +111,3 @@ int _myalias(info_t *info)
 
 	return (0);
 }
-
